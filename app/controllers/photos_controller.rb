630f3db2203a104ee77579b543aeabd0c53b5e97
mstenclik <michael.stenclik@gmail.com>
2015-05-10 15:00:31 -0500

    Photogram_Up to Update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @show_photos = Photo.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+  end
+
+  def create_row
+    create_photo = Photo.new
+    create_photo.caption = params["the_caption"]
+    create_photo.source = params["the_source"]
+    create_photo.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @destroy_photo = Photo.find_by({ :id => params[:id] }).destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @edit_photo = Photo.find_by({ :id => params[:id] })
+    @edit_photo.caption = params["the_caption"]
+    @edit_photo.source = params["the_source"]
+    @edit_photo.save
+    redirect_to
+  end
 
 end
